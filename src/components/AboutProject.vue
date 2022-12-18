dd3b217fcc2425bd8e060148798313820526a62c
tinatrinh1012 <nhungoctrinhle@gmail.com>
2022-12-18 16:50:34 -0600

    About Project page

---
+++ src/components/AboutProject.vue
@@ -0,0 +1,68 @@
+<template>
+    <h2>Team Members</h2>
+    <div class="grid-x">
+        <div class="cell small-12 medium-4">
+            <img src="../images/tina.jpg" alt="Tina Portrait">
+        </div>
+        <div class="cell small-12 medium-8">
+            <h4>Tina Trinh</h4>
+            <p>
+                Current senior, major in Computer Science with minors in Data Analytics and Applied Statistics.
+                I'm from Ho Chi Minh, Vietnam. My hobby is singing, dancing, and listening to music.
+            </p>
+        </div>
+    </div>
+
+    <div class="grid-x">
+        <div class="cell small-12 medium-4">
+            <img src="../images/tina.jpg" alt="Tina Portrait">
+        </div>
+        <div class="cell small-12 medium-8">
+            <h4>Aashish Bharath</h4>
+            <p>
+                Aashish Brarath
+            </p>
+        </div>
+    </div>
+
+    <div class="grid-x">
+        <div class="cell small-12 medium-4">
+            <img src="../images/tina.jpg" alt="Tina Portrait">
+        </div>
+        <div class="cell small-12 medium-8">
+            <h4>Sunisha Arora</h4>
+            <p>
+                Sunisha Arora
+            </p>
+        </div>
+    </div>
+
+    <h2>Tools/Frameworks</h2>
+    <h4>Leaflet API</h4>
+    <p></p>
+    <h4>Nominatim API</h4>
+    <p></p>
+    <h4>Vue.js</h4>
+    <p>
+        We used Vue.js framework to manage our front-end app structure and logic.
+        Vue.js allowed us to manage and update app state efficiently through data binding and lifecycle hooks.
+    </p>
+
+    <h2>Video Demo</h2>
+
+    <h2>Interesting Things We Learned</h2>
+    <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ol>
+</template>
+
+<style>
+    .grid-x {
+        padding-bottom: 2rem;
+    }
+</style>
